c08bfaab91af2280bc68b202c263bdc1de90b5c1
Guch1120 <e1x22089@oit.ac.jp>
2025-08-19 17:22:58 +0900

    flexbeの起動を確認．colcon buildをしていなくて/install/setup.bashが存在していないことが原因だった． dockefileとlaunchファイルにビルドするように変更を加えた

--- scripts/launch_1.bash
+++ scripts/launch_1.bash
@@ -2,9 +2,29 @@
 echo "launch gazebo"
 # ROS2 Humbleの環境設定を読み込む
 source /opt/ros/humble/setup.bash
-if [ -f "/opt/ros2_ws/install/setup.bash" ]; then
-    source /opt/ros2_ws/install/setup.bash
+
+# ワークスペースのセットアップファイルを確認し、なければビルドする
+WS_PATH="/home/dockeruser/ros2_ws"
+SETUP_FILE="$WS_PATH/install/setup.bash"
+
+if [ -f "$SETUP_FILE" ]; then
+    echo "Sourcing existing workspace: $SETUP_FILE"
+    source "$SETUP_FILE"
+else
+    echo "Workspace setup file not found. Attempting to build workspace in '$WS_PATH'..."
+    if [ -d "$WS_PATH/src" ]; then
+        (cd "$WS_PATH" && colcon build --symlink-install)
+        if [ -f "$SETUP_FILE" ]; then
+            echo "Build successful. Sourcing new setup file."
+            source "$SETUP_FILE"
+        else
+            echo "Build failed or setup file was not created. Gazebo may not find custom packages."
+        fi
+    else
+        echo "Workspace 'src' directory not found in '$WS_PATH'. Cannot build."
+    fi
 fi
+
 
 # Ctrl+C (SIGINT) を無視する
 trap '' INT
